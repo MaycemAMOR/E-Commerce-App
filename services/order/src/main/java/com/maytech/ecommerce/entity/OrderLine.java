e8f21b95587e4af6635f61d99697d6489652a56c
Maycem AMOR <maycemamor@gmail.com>
2024-06-05 22:33:30 +0200

    - create  the entities : order & orderLine - create the enum paymentMethod

---
+++ services/order/src/main/java/com/maytech/ecommerce/entity/OrderLine.java
@@ -0,0 +1,21 @@
+package com.maytech.ecommerce.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+@Entity
+public class OrderLine {
+    @Id
+    @GeneratedValue
+    private Integer id;
+    @ManyToOne
+    @JoinColumn(name = "order_id") // name column tablename_primaryKey
+    private Order order;
+    private Integer productId;
+    private double quantity;
+}
